c01f84335cd7ce599f5026073d00024da17fe92c
Mike Morrone <Hanegwen725@gmail.com>
2019-04-01 13:34:47 -0500

    plugin tests

---
+++ LuminalMuseumVR/Assets/Scripts/VRController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class VRController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.XR.XRSettings.enabled = true;
+        //VRSettings.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
