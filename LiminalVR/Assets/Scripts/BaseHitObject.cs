8b893aed99dbb591c84ce924aed79bf3926fab58
Mike Morrone <Hanegwen725@gmail.com>
2019-04-02 14:38:55 -0500

    Sorry there is a lot of poking

---
+++ LiminalVR/Assets/Scripts/BaseHitObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseHitObject : MonoBehaviour, IHitable
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Hit()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ LiminalVR/Assets/Scripts/Gun/BaseGun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseGun : MonoBehaviour
+{
+    bool canShoot = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //I'm guessing Input here is a page for it
+        //https://developer.oculus.com/documentation/unity/latest/concepts/unity-ovrinput/
+        if (OVRInput.Get(OVRInput.Button.PrimaryHandTrigger))
+        {
+
+        }
+    }
+
+    protected virtual void Shoot()
+    {
+        if(canShoot)
+        {
+
+        }
+    }
+}
