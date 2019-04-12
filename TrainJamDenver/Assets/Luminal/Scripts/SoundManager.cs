c0bd47a6457ebfddad20b6cf96474de9048355f4
Mike Morrone <Hanegwen725@gmail.com>
2019-04-12 18:11:24 -0500

    Sounds

---
+++ TrainJamDenver/Assets/Luminal/Scripts/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource backgroundMusic;
+
+    [SerializeField]
+    AudioSource EffectSounds;
+
+    [SerializeField]
+    AudioClip SpringHammerSound;
+
+    [SerializeField]
+    AudioClip StartSound;
+
+    [SerializeField]
+    AudioClip CompleteSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundMusic.loop = true;
+        backgroundMusic.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BeginGame()
+    {
+        EffectSounds.clip = StartSound;
+        EffectSounds.Play();
+    }
+}
