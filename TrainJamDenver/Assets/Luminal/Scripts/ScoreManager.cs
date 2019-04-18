4a1c892e4a4a5e7546887fe739337c87a07ef955
Mike Morrone <Hanegwen725@gmail.com>
2019-04-17 21:02:37 -0500

    Working on Animations

--- TrainJamDenver/Assets/Luminal/Scripts/ScoreManager.cs
+++ TrainJamDenver/Assets/Luminal/Scripts/ScoreManager.cs
@@ -2,17 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
 
 public class ScoreManager : MonoBehaviour
 {
 
     [SerializeField]
-    TextMeshPro WallText;
+    List<TextMeshPro> WallText;
 
     [SerializeField]
     TextMeshPro FinalScoreText;
 
     float currentscore;
+
+    [SerializeField]
+    float MoveText = 2;
+
+    [SerializeField]
+    float jumpPower = 1;
+
+    [SerializeField]
+    int numJumps = 2;
+
+    [SerializeField]
+    float duration = 6;
+
+    [SerializeField]
+    float DelayForReversal = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,8 +47,16 @@ public class ScoreManager : MonoBehaviour
 
     public void UpdateScore(float ScoreToAdd)
     {
-        currentscore += ScoreToAdd;
 
-        WallText.text = currentscore.ToString();
+        foreach (TextMeshPro wall in WallText)
+        {
+            Vector3 baseLocation = wall.transform.position;
+            currentscore += ScoreToAdd;
+            wall.transform.DOJump(new Vector3(wall.transform.position.x - MoveText, wall.transform.position.y, wall.transform.position.z - MoveText), jumpPower, numJumps, duration);
+            wall.text = currentscore.ToString();
+
+
+            wall.transform.DOJump(baseLocation, jumpPower, numJumps, duration).SetDelay(DelayForReversal);
+        }
     }
 }
