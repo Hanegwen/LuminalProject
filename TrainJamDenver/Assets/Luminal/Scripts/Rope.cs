ddb3c0a884fa9e885a94e9b8caf4fb9b923552dd
Mike Morrone <Hanegwen725@gmail.com>
2019-05-01 11:35:57 -0500

    Physics are great

---
+++ TrainJamDenver/Assets/Luminal/Scripts/Rope.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.GetComponent<Bullet>() != null)
+        {
+            rigidbody.useGravity = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<Bullet>() != null)
+        {
+            rigidbody.useGravity = true;
+        }
+    }
+}
